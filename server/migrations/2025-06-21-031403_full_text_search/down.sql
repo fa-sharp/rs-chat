745a6b8a2a46774016ca3a2485d632ba7a43c400
fa-sharp <farshadkt@gmail.com>
2025-06-22 12:56:00 -0700

    server: format SQL

--- server/migrations/2025-06-21-031403_full_text_search/down.sql
+++ server/migrations/2025-06-21-031403_full_text_search/down.sql
@@ -1,7 +1,10 @@
 DROP TRIGGER chat_sessions_search_vector_update on chat_sessions;
+
 DROP TRIGGER chat_messages_search_vector_update on chat_messages;
 
 DROP FUNCTION chat_sessions_search_vector_update;
+
 DROP FUNCTION chat_messages_search_vector_update;
 
-ALTER TABLE chat_messages DROP COLUMN search_vector;
+ALTER TABLE chat_messages
+DROP COLUMN search_vector;
