19caaf4541b1dc479ecd1dfc3cd44be96fb9fb30
fa-sharp <farshadkt@gmail.com>
2026-06-23 01:58:48 -0400

    rename /routes to /api

--- server-new/src/lib.rs
+++ server-new/src/lib.rs
@@ -2,12 +2,12 @@ use axum_plugin::{App, InitializedApp};
 
 use crate::state::AppState;
 
+mod api;
 mod config;
 mod db;
 mod error;
 mod extractors;
 mod plugins;
-mod routes;
 mod services;
 mod state;
 
@@ -16,7 +16,7 @@ pub async fn create_app() -> anyhow::Result<InitializedApp<AppState>> {
         .register(config::plugin()) // Extract configuration and add to state
         .register(plugins::database::plugin()) // Initialize database
         .register(plugins::redis::plugin()) // Initialize Redis
-        .register(routes::plugin()) // Add API routes
+        .register(api::plugin()) // Add API routes
         .register(plugins::session::plugin()) // Setup sessions
         .register(plugins::logging::plugin()) // Request logging
         .register(plugins::security::plugin()) // Body limit, security headers, etc.
