b651198785acb21dcef70b4d6ffaf4aada08e3fd
fa-sharp <farshadkt@gmail.com>
2026-06-23 01:56:27 -0400

    redis shutdown

--- server-new/src/db/repositories/user.rs
+++ server-new/src/db/repositories/user.rs
@@ -25,16 +25,16 @@ impl<'a> UserRepository<'a> {
         Ok(user)
     }
 
-    // pub async fn find_by_github_id(&mut self, id: &str) -> Result<Option<ChatRsUser>, Error> {
-    //     let user = users::table
-    //         .filter(users::github_id.eq(id))
-    //         .select(ChatRsUser::as_select())
-    //         .first(self.db)
-    //         .await
-    //         .optional()?;
+    pub async fn find_by_github_id(&mut self, id: &str) -> Result<Option<ChatRsUser>, Error> {
+        let user = users::table
+            .filter(users::github_id.eq(id))
+            .select(ChatRsUser::as_select())
+            .first(self.db)
+            .await
+            .optional()?;
 
-    //     Ok(user)
-    // }
+        Ok(user)
+    }
 
     // pub async fn find_by_google_id(&mut self, id: &str) -> Result<Option<ChatRsUser>, Error> {
     //     let user = users::table
--- server-new/src/plugins/redis.rs
+++ server-new/src/plugins/redis.rs
@@ -9,25 +9,37 @@ use crate::{config::AppConfig, db::DbPool, state::AppState};
 const DEFAULT_TIMEOUT: Duration = Duration::from_secs(8);
 
 pub fn plugin() -> AdHocPlugin<AppState> {
-    AdHocPlugin::named("Redis").on_init(async |mut state| {
-        let app_config = state.get::<AppConfig>().context("no config")?;
-        let db_pool = state.get::<DbPool>().context("no database pool")?;
-        let config = Config::from_url(&app_config.redis.url).context("invalid Redis URL")?;
-        let pool = Builder::from_config(config)
-            .with_connection_config(|c| {
-                c.connection_timeout = DEFAULT_TIMEOUT;
-                c.internal_command_timeout = DEFAULT_TIMEOUT;
-                c.tcp.nodelay = Some(true);
-            })
-            .with_performance_config(|c| {
-                c.default_command_timeout = DEFAULT_TIMEOUT;
-            })
-            .build_pool(db_pool.status().max_size)?; // same size as database pool
+    AdHocPlugin::named("Redis")
+        .on_init(async |mut state| {
+            let app_config = state.get::<AppConfig>().context("no config")?;
+            let db_pool = state.get::<DbPool>().context("no database pool")?;
+            let config = Config::from_url(&app_config.redis.url).context("invalid Redis URL")?;
+            let pool = Builder::from_config(config)
+                .with_connection_config(|c| {
+                    c.connection_timeout = DEFAULT_TIMEOUT;
+                    c.internal_command_timeout = DEFAULT_TIMEOUT;
+                    c.tcp.nodelay = Some(true);
+                })
+                .with_performance_config(|c| {
+                    c.default_command_timeout = DEFAULT_TIMEOUT;
+                })
+                .build_pool(db_pool.status().max_size)?; // same size as database pool
 
-        pool.init().await.context("failed to connect to Redis")?;
-        tracing::info!("Connected to Redis");
+            pool.init().await.context("failed to connect to Redis")?;
+            tracing::info!("Connected to Redis");
 
-        state.insert(pool);
-        Ok(state)
-    })
+            state.insert(pool);
+            Ok(state)
+        })
+        .on_shutdown(|state: &AppState| {
+            let pool = state.redis.clone();
+            async move {
+                if let Err(e) = pool.quit().await {
+                    tracing::warn!("Error shutting down Redis pool: {e}");
+                } else {
+                    tracing::info!("Shut down Redis pool")
+                }
+                Ok(())
+            }
+        })
 }
